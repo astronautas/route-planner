7041b76d9fbb55133af66be132b637f65f632790
Rytis Kaplūnas <rytkaplunas@gmail.com>
2016-12-01 19:28:44 +0200

    Android app project created.

---
+++ mobileApp/mobileApp/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace mobileApp
+{
+    [Activity(Label = "mobileApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            int count = 1;
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
