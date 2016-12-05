efc9bf4d849ec444200fee6ebbc0cb78bc517b0e
Rytis Kaplūnas <rytkaplunas@gmail.com>
2016-12-05 15:37:07 +0200

    Started to work on SignIn and SignUp

--- mobileApp/mobileApp/MainActivity.cs
+++ mobileApp/mobileApp/MainActivity.cs
@@ -1,22 +1,78 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
+using System.Threading;
+using Android.Views;
 
 namespace mobileApp
 {
     [Activity(Label = "mobileApp", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Button _btnSignUp;
+        private Button _btnSignIn;
+        private ProgressBar _progressBar;
+
         protected override void OnCreate(Bundle bundle)
         {
-            int count = 1;
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
 
-            //Button button = FindViewById<Button>(Resource.Id.button1);
-            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            _btnSignUp = FindViewById<Button>(Resource.Id.buttonSignUp);
+            _btnSignIn = FindViewById<Button>(Resource.Id.buttonSignIn);
+
+            _progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+
+            _btnSignUp.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                DialogSignUp signUpDialog = new DialogSignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+
+                signUpDialog._onSignUpComplete += OnSignUp;
+            };
+
+            _btnSignIn.Click += (object sender, EventArgs args) =>
+            {
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                DialogSignIn signInDialog = new DialogSignIn();
+                signInDialog.Show(transaction, "dialog fragment");
+
+                signInDialog._onSignInComplete += OnSignIn;
+            };
+
+
+
+        }
+
+
+        void OnSignUp(object sender, OnSignUpEventArgs args)
+        {
+            _progressBar.Visibility = ViewStates.Visible;
+            Thread thread = new Thread(() => RequestSignUp(args));
+            thread.Start();
+
+        }
+
+        void OnSignIn(object sender, OnSignInEventArgs args)
+        {
+            _progressBar.Visibility = ViewStates.Visible;
+            Thread thread = new Thread(() => RequestSignIn(args));
+            thread.Start();
+        }
+        //Po sign up dialoge nuspaudimo gaunam user input reiktu irasyt i serveri ar patikrint
+        private void RequestSignUp(OnSignUpEventArgs args)
+        {
+            Thread.Sleep(3000);
+            RunOnUiThread(() => _progressBar.Visibility = ViewStates.Invisible);
+        }
+        private void RequestSignIn(OnSignInEventArgs args)
+        {
+            Thread.Sleep(3000);
+            RunOnUiThread(() => _progressBar.Visibility = ViewStates.Invisible);
         }
     }
 }
