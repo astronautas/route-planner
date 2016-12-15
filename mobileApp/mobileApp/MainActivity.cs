760fe1a4837e0709420beb104890b1157cac0954
Tomaš Monkevič <Toma Monkevi>
2016-12-15 19:11:03 +0200

    Importing Google Maps

--- mobileApp/mobileApp/MainActivity.cs
+++ mobileApp/mobileApp/MainActivity.cs
@@ -4,22 +4,39 @@ using Android.OS;
 using System;
 using System.Threading;
 using Android.Views;
+using Android.Gms.Maps;
 
 namespace mobileApp
 {
     [Activity(Label = "mobileApp", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, IOnMapReadyCallback
     {
+        public GoogleMap mMap;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
+            SetUpMap();
 
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetActionBar(toolbar);
             ActionBar.Title = "Route Planner";
+            
+        }
+
+        private void SetUpMap()
+        {
+            if(mMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mMap = googleMap;
         }
     }
 }
