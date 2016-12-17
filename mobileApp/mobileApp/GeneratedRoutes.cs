46486e2906f2a19f6c41a48a9cc377d579e0447c
Tomaš Monkevič <Toma Monkevi>
2016-12-17 21:58:15 +0200

    Fixing merges

--- mobileApp/mobileApp/GeneratedRoutes.cs
+++ mobileApp/mobileApp/GeneratedRoutes.cs
@@ -10,6 +10,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Gms.Maps;
+using Android.Graphics;
 
 namespace mobileApp
 {
@@ -18,6 +19,8 @@ namespace mobileApp
     {
         public GoogleMap mMap;
         MapFragment mapFragment;
+        private ScrollView _generatedRoutesView;
+        private List<RouteToDisplay> _generatedRoutes = new List<RouteToDisplay>(); //temp
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -31,6 +34,52 @@ namespace mobileApp
             
 
             SetUpMap();
+            _generatedRoutesView = FindViewById<ScrollView>(Resource.Id.routesScrollView);
+            _generatedRoutes.Add(new RouteToDisplay("Vilnius", "Siauliai", "Klaipeda", "11"));
+            _generatedRoutes.Add(new RouteToDisplay("Ukmerge", "Panevezys", "Palanga", "15"));
+            _generatedRoutes.Add(new RouteToDisplay("Kaunas", "Trakai", "Telsiai", "8"));
+            _generatedRoutes.Add(new RouteToDisplay("Vilnius", "Ukmerge", "Taujenai", "5"));
+            _generatedRoutes.Add(new RouteToDisplay("Kaunas", "Trakai", "Telsiai", "8"));
+            _generatedRoutes.Add(new RouteToDisplay("Vilnius", "Ukmerge", "Taujenai", "5"));
+
+            ShowGeneratedRoutes();
+        }
+
+        private void ShowGeneratedRoutes() //drawing UI elements needs to be fixed
+        {
+            TableLayout layout = FindViewById<TableLayout>(Resource.Id.routeList);
+            foreach (var route in _generatedRoutes)
+            {
+                var text = new TextView(this);
+                text.Text = route.ToString();
+                text.SetWidth(900);
+                text.SetTextColor(new Color(252, 177, 80)); //our orange
+                text.SetTextSize(Android.Util.ComplexUnitType.Pt, 10);
+                //text.SetWidth();
+                //text.SetHeight();
+
+                var button = new Button(this);
+                button.Text = "Pirkti";
+                button.Background = Resources.GetDrawable(Resource.Drawable.OrangeActionBtn); //deprecated but fuck it
+                button.SetTextColor(Color.White);
+
+                var tableRow = new TableRow(this);
+                tableRow.SetMinimumHeight(250);
+                tableRow.AddView(text);
+                tableRow.AddView(button);
+
+                layout.AddView(tableRow);
+            }
+        }
+
+        private void Order(Route route)
+        {
+            string output = string.Empty;
+            //string output = JsonConvert.SerializeObject(route);
+
+            var activity2 = new Intent(this, typeof(OrderActivity));
+            activity2.PutExtra("ChosenRoute", output);
+            StartActivity(activity2);
         }
 
         private void SetUpMap()
